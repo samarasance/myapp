326989dade535845e892e5a65e401ae1216d27b0
samarasance <88958996+samarasance@users.noreply.github.com>
2021-11-03 21:18:02 -0500

    added description element to JS

--- src/script.js
+++ src/script.js
@@ -2,8 +2,10 @@ function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
 
 let apiKey = "292929ff665169ef5a98dcc8cc29979a";
