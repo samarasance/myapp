cba02c6aecae43e71377d56823c64fc0dcc72499
samarasance <88958996+samarasance@users.noreply.github.com>
2021-11-07 13:13:07 -0600

    added humidity api

--- src/script.js
+++ src/script.js
@@ -3,9 +3,11 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
 }
 
 let apiKey = "292929ff665169ef5a98dcc8cc29979a";
