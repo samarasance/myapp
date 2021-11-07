de48c65e82ce0eaf72b4b9eac5e8d2678df3cd45
samarasance <88958996+samarasance@users.noreply.github.com>
2021-11-07 14:32:48 -0600

    added sunrise and sunset updated css

--- src/script.js
+++ src/script.js
@@ -4,10 +4,17 @@ function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let sunriseElement = document.querySelector("#sunrise");
+  let sunsetElement = document.querySelector("#sunset");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  sunriseElement.innerHTML = response.data.sys.sunrise;
+  sunsetElement.innerHTML = response.data.sys.sunset;
 }
 
 let apiKey = "292929ff665169ef5a98dcc8cc29979a";
