582d531d890e4e9e1ac9fe0cd021770875e30319
samarasance <88958996+samarasance@users.noreply.github.com>
2021-11-12 20:29:41 -0600

    correcting js date code

--- src/script.js
+++ src/script.js
@@ -1,5 +1,44 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours > 10) {
+    hours = `${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes > 10) {
+    minutes = `${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day}
+   ${hours}:${minutes}`;
+}
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
@@ -7,6 +46,7 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   let sunriseElement = document.querySelector("#sunrise");
   let sunsetElement = document.querySelector("#sunset");
+  let dateElement = document.querySelector("#date");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
@@ -15,9 +55,13 @@ function displayTemperature(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   sunriseElement.innerHTML = response.data.sys.sunrise;
   sunsetElement.innerHTML = response.data.sys.sunset;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
+let h2 = document.querySelector("h2");
+h2.innerHTML = "JUmble";
+
 let apiKey = "292929ff665169ef5a98dcc8cc29979a";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Miami&appid=${apiKey}&units=imperial`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Washington DC&appid=${apiKey}&units=imperial`;
 
 axios.get(apiUrl).then(displayTemperature);
